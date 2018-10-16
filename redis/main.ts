d74a90890337762603bf4384c4cd6dfa64028921
jnst <jnst@outlook.com>
2018-10-16 15:55:15 +0900

    Update redis

--- redis/main.ts
+++ redis/main.ts
@@ -2,10 +2,10 @@ import * as Redis from "ioredis";
 import * as logger from 'signale';
 
 (async () => {
-  const redis = new Redis('127.0.0.1');
+  const redis = new Redis(); // 127.0.0.1:6379
 
-  const result = await redis.ping();
-  logger.success(result);
+  const pong = await redis.ping();
+  logger.success(pong);
 
   redis.disconnect();
 })();
